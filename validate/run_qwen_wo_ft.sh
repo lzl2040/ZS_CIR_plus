3ce827ced2a6bb4f01949f1bf5669d7a6de0f74c
lzl2040 <2040171586@qq.com>
2025-08-04 08:49:13 +0000

    qwen_wo_ft_cirr

---
+++ validate/run_qwen_wo_ft.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# 你的 checkpoint ID 列表
+CKPT_LIST=(
+  checkpoint-40
+)
+
+# 其他参数
+BASE_PATH="/mnt/input_zuo/ZS-CIR/plus_version/saves/qwen-4bit-ke"
+# BASE_PATH="/home/v-zuoleili/Temp/ZS_CIR_plus/phi3-4bit-ke"
+PROMPT_TYPES=("ke" "cot" "org")
+
+# 遍历 checkpoint
+for CKPT_ID in "${CKPT_LIST[@]}"; do
+  WEIGHT_ROOT="$BASE_PATH/$CKPT_ID"
+  echo "Testing checkpoint: $WEIGHT_ROOT"
+
+  # 遍历 prompt_type
+  for PROMPT in "${PROMPT_TYPES[@]}"; do
+    echo "  Using prompt_type: $PROMPT"
+
+    CUDA_VISIBLE_DEVICES=0 python src/sft_qwen_embed.py \
+      --name 'qwen2_5_vl' \
+      --base_model 'qwen2_5_vl' \
+      --prompt_type "$PROMPT" \
+      --file_path '/mnt/input_zuo/ZS-CIR/plus_version/results_wo_ft' \
+      --shared_concept
+  done
+done
--- validate/src/sft_qwen_embed.py
+++ validate/src/sft_qwen_embed.py
@@ -590,10 +590,10 @@ def main(
     model, transform = init_model_and_transform(base_model, lora_path, bf16)
     model.to(device)
 
-    datasets = ['circo']
+    # datasets = ['circo']
     # datasets = ['fashioniq dress', 'fashioniq shirt', 'fashioniq toptee', 'circo']
     # datasets = ['fashioniq dress', 'fashioniq shirt', 'fashioniq toptee']
-    # datasets = ['cirr']
+    datasets = ['cirr']
     print(f"Test datasets:{datasets}")
     if data:
         datasets = data.split(',')
